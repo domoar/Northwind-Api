7599421e1552628deffb3310568bcfb17911367f
ManuelDausmann <156981587+domoar@users.noreply.github.com>
2025-04-22 14:30:51 +0200

    apifactory using testcontainers

--- tests/IntegrationTests/ApiFactory.cs
+++ tests/IntegrationTests/ApiFactory.cs
@@ -1,23 +1,55 @@
 ﻿using Api;
+using Infrastructure.context;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Testcontainers.PostgreSql;
 
 namespace IntegrationTests;
 public class NorthwindApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime {
+
+private readonly ILogger<NorthwindApiFactory> _logger;
+private readonly PostgreSqlContainer _databaseContainer;
+public NorthwindApiFactory(){
+
+  using var loggerFactory = LoggerFactory.Create(builder =>
+  {
+      builder
+        .AddConsole()
+        .AddDebug()
+        .SetMinimumLevel(LogLevel.Information);
+  });
+
+  _logger = loggerFactory.CreateLogger<NorthwindApiFactory>();
+
+  _databaseContainer =new PostgreSqlBuilder()
+    .WithImage("")
+    .WithName("")
+    .WithCleanUp(true)
+    .WithPortBinding(8888, 8888)
+    .Build();
+}
+
   public async Task InitializeAsync() {
-    await Task.Delay(1);
-    //TODO add test fixture
+    await _databaseContainer.StartAsync();
+
+
   }
 
   async Task IAsyncLifetime.DisposeAsync() {
-    await Task.Delay(1);
-    //TODO add test teardown
+    await _databaseContainer.StopAsync();
   }
 
+  protected override void ConfigureWebHost(IWebHostBuilder builder) {
+    var integrationTestConnection = _databaseContainer.GetConnectionString();
+    //TODO inject new connection to webhost without breaking the cfg
+    base.ConfigureWebHost(builder);
+  }
   [CollectionDefinition("ApiFactory context collection")]
   public class SharedApiFactoryFixtureCollection : ICollectionFixture<NorthwindApiFactory> { }
 }
