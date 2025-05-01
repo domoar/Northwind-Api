c23bcfd32f8b67c3fd07db5f608812ff7b78e8a7
domoar <156981587+domoar@users.noreply.github.com>
2025-05-01 11:46:08 +0000

    ci: apply dotnet format

--- src/Api/Request/Validation/CustomerRequestValidator.cs
+++ src/Api/Request/Validation/CustomerRequestValidator.cs
@@ -4,22 +4,22 @@ using FluentValidation;
 namespace Api.Request.Validation;
 
 public class CustomerRequestValidator : AbstractValidator<CustomerRequest> {
-    public const int MAX_ID_LENGTH = 55;
+  public const int MAX_ID_LENGTH = 55;
 
-    public CustomerRequestValidator() {
-        RuleFor(x => x.CustomerID)
-            .Cascade(CascadeMode.Stop)
-            .NotNull()
-                .WithErrorCode("CustomerID.NotNull")
-                .WithMessage("Customer Id cannot be null")
-            .NotEmpty()
-                .WithErrorCode("CustomerID.NotEmpty")
-                .WithMessage("Customer Id cannot be empty")
-            .Must(id => string.IsNullOrWhiteSpace(id) == false)
-                .WithErrorCode("CustomerID.WhitespaceOnly")
-                .WithMessage("Customer Id cannot be whitespace")
-            .MaximumLength(MAX_ID_LENGTH)
-                .WithErrorCode("CustomerID.TooLong")
-                .WithMessage($"Customer Id cannot be longer can {MAX_ID_LENGTH}");
-    }
+  public CustomerRequestValidator() {
+    RuleFor(x => x.CustomerID)
+        .Cascade(CascadeMode.Stop)
+        .NotNull()
+            .WithErrorCode("CustomerID.NotNull")
+            .WithMessage("Customer Id cannot be null")
+        .NotEmpty()
+            .WithErrorCode("CustomerID.NotEmpty")
+            .WithMessage("Customer Id cannot be empty")
+        .Must(id => string.IsNullOrWhiteSpace(id) == false)
+            .WithErrorCode("CustomerID.WhitespaceOnly")
+            .WithMessage("Customer Id cannot be whitespace")
+        .MaximumLength(MAX_ID_LENGTH)
+            .WithErrorCode("CustomerID.TooLong")
+            .WithMessage($"Customer Id cannot be longer can {MAX_ID_LENGTH}");
+  }
 }
