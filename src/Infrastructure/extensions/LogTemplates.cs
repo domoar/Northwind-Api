456e7e4221aca5bd93fbd78fc538c7a5cd71c545
ManuelDausmann <156981587+domoar@users.noreply.github.com>
2025-05-01 13:25:33 +0200

    adjusted directories case match

--- src/Infrastructure/extensions/LogTemplates.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace Infrastructure.Extension;
-public static partial class LogTemplates {
-  [LoggerMessage(EventId = 401, Level = LogLevel.Information, Message = "", SkipEnabledCheck = true)]
-  public static partial void LogFoo(this ILogger logger);
-}
